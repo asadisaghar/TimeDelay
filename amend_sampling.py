a74419c333e50f793a13e868814a7e1189943eea
Egil <egil.moller@freecode.no>
2016-04-19 23:36:32 +0200

    Add sampling window ids to the data

---
+++ amend_sampling.py
@@ -0,0 +1,28 @@
+import numpy as np
+from timedelay.sampling_windows import *
+from numpy.lib.recfunctions import *
+
+data = np.load("pairs_with_truths.npz")['arr_0']
+pair_ids = np.unique(data['full_pair_id'])
+
+data = append_fields(data, 'window_id', [], dtypes='<f8')
+data = data.filled()
+
+window_id = 0
+for pair_id in pair_ids:
+    pair_query = data['full_pair_id'] == pair_id
+    pair_data = data[pair_query]
+    
+    times = data[pair_query]['time']
+    times.sort()
+    
+    time_windows = times[pick_sampling_windows(times)]
+    
+    for idx in xrange(0, len(time_windows)):
+        window_start, window_end = time_windows[idx]
+        data['window_id'][pair_query
+                          & (data['time'] >= window_start)
+                          & (data['time'] <= window_end)] = window_id
+        window_id += 1
+
+np.savez("pairs_with_truths_and_windows.npz", data)
--- timedelay/sampling_windows.py
+++ timedelay/sampling_windows.py
@@ -6,13 +6,15 @@ from sklearn.gaussian_process import GaussianProcess
 def pick_sampling_windows(timestamps):
     """Find border indices of sampling windows in data"""
 
-    dt = []
-    for i in xrange(1, len(timestamps)):
-        dt.append(timestamps[i]-timestamps[i-1])
-    dt_threshold = np.mean(dt)*1.
+    dt = timestamps[1:] - timestamps[:-1]
+
+    dt_threshold = (np.mean(dt) + np.max(dt)) / 2.
+
     right_ends = np.append(np.where(dt>=dt_threshold)[0], len(timestamps)-1)
     left_ends = np.append(0, np.where(dt>=dt_threshold)[0]+1)
-    windows = np.zeros((len(right_ends), 2))
-    for i in range(0, len(right_ends)):
-        windows[i] = (left_ends[i], right_ends[i])
+
+    windows = np.zeros((len(right_ends), 2), dtype='i4')
+    windows[:,0] = left_ends
+    windows[:,1] = right_ends
+
     return windows
