d3da6641c196aba77e33d594dcdd794fdb44e555
asadisaghar <asadisaghar@gmail.com>
2016-04-20 11:37:40 +0200

    GP is broken!

---
+++ LassoReg.py
@@ -0,0 +1,3 @@
+# Perform a Lasso regression on the entire dataset
+# Features: N_eval*len(windows) encoding residuals of detrend(brighter lightcurve)-detrend(fainter lightcurve)
+# labels: dt_true, tau, sigma[, m1, m2]
