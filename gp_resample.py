fbb15296d042e92c182146ff26d8675d90068929
asadisaghar <asadisaghar@gmail.com>
2016-04-24 13:47:38 +0200

    function added to split data with windows

--- gp_resample.py
+++ gp_resample.py
@@ -10,7 +10,7 @@ system_type="double"
 data = np.load("TimeDelayData/pairs_with_truths_and_windows.npz")['arr_0']
 pair_ids = np.unique(data['full_pair_id'])
 pair_ids = filter_pairs(pair_ids, system_type)
-pair_ids = np.intersect1d(pair_ids, np.unique(data[data['window_id'] < 2320.0]['full_pair_id']))
+pair_ids = np.intersect1d(pair_ids, np.unique(data[data['window_id'] < 2770.0]['full_pair_id']))
 
 outdata = resample_using_gp_models(data, pair_ids)
 np.savez('TimeDelayData/gp_resampled_of_windows_with_truth', outdata)
