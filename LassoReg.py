29ff8ca3fdb9cff7c280b33225b927240326995f
asadisaghar <asadisaghar@gmail.com>
2016-04-24 15:20:15 +0200

    Lasso fixes

--- LassoReg.py
+++ LassoReg.py
@@ -10,51 +10,37 @@ from sklearn.cross_validation import train_test_split
 from timedelay.data_splitting import *
 
 data = np.load("TimeDelayData/gp_resampled_of_windows_with_truth.npz")['arr_0']
-
+print "data shape: ", data.shape
 feature_names = ['sig_evalA', 'sig_errA', 'sig_evalB', 'sig_errB']
 label_names = ['dt', 'tau', 'sig', 'm1', 'm2']
 feature_formats = ['float64'] * len(feature_names)
 label_formats = ['float64'] * len(label_names)
 
-# # Read fetures
-# features = np.zeros(len(data), dtype=[('sig_evalA', np.float64), 
-#                                       ('sig_errA', np.float64),
-#                                       ('sig_evalB', np.float64),
-#                                       ('sig_errB', np.float64)])
-
-# # Read lables
-# labels = np.zeros(len(data), dtype=[('dt', np.float64),
-#                                     ('tau', np.float64),
-#                                     ('sig', np.float64),
-#                                     ('m1', np.float64),
-#                                     ('m2', np.float64)])
-
 # Read fetures
-features = np.zeros(len(data), dtype={'names': feature_names, 
-                                      'formats': feature_formats})
+features = np.zeros((len(data), len(feature_names)))
 
 # Read lables
-labels = np.zeros(len(data), dtype={'names': label_names, 
-                                    'formats': label_formats})
+labels = np.zeros((len(data), len(label_names)))
 
+for i, name in enumerate(feature_names):
+    features[:, i] = data[name]
+print "features shape: ", features.shape
 
-for name in feature_names:
-    features[name] = data[name]
+for j, name in enumerate(label_names):
+    labels[:, j] = data[name]
+print "labels shape: ", labels.shape
 
-for name in label_names:
-    labels[name] = data[name]
-
-features_train, labels_train, features_test, labels_test = split_data_by_windows(data, test_fraction=0.33)
+features_train, labels_train, features_test, labels_test = split_data_by_windows(data, features, labels, test_fraction=0.33)
 
 # Guess a range of alphas (regularization parameter)
-alphas = 10**np.linspace(-5, 1, 100)
+alphas = 10 ** np.linspace(-5, 1, 100)
 
 # Build a LassoCV model
 # Train a Lasso model over alphas and choose the best alpha by cross-validation
 model = LassoCV(alphas=alphas, selection='random', fit_intercept=True, normalize=True) 
-model.fit(features_train['sig_evalA'], labels_train['dt'])
+model.fit(features_train, labels_train[:, 0])
 w = model.coef_
-
+print w
 # Plot feature contributions for the best-fit model asd the MSE for each alpha
 fig, axs = plt.subplots(1,3)
 axs = axs.flatten()
@@ -63,7 +49,7 @@ axs[0].plot(model.alphas_, model.mse_path_)
 axs[1].plot(range(len(w)), w, 'o')
 
 # How well does the trained model perform on the test test?
-labels_pred = model.predict(features_test['sig_evalA'])
-score = np.mean((labels_pred - labels_test['dt']) ** 2)
-axs[2].plot(labels_pred, labels_test['dt'], '.')
-axs[2].plot(labels_test['dt'], labels_test['dt'], '-k') 
+labels_pred = model.predict(features_test)
+score = np.mean((labels_pred - labels_test[:, 0]) ** 2)
+axs[2].plot(labels_pred, labels_test[:, 0], '.')
+axs[2].plot(labels_test[:, 0], labels_test[:, 0], '-k') 
--- timedelay/data_splitting.py
+++ timedelay/data_splitting.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def split_data_by_windows(data, test_fraction=0.33):
+def split_data_by_windows(data, features, labels, test_fraction=0.33):
     # Split data to train and test sets by window_id
     # Cross validation is done inside LassoCV, so no need for a separate CV set
     windows = np.unique(data['window_id'])
@@ -8,10 +8,12 @@ def split_data_by_windows(data, test_fraction=0.33):
     test_windows = windows[:test_size]
     train_windows = windows[test_size + 1:]
     test_end_ind = np.where(data['window_id'] == test_windows[-1])[0][-1]
-    features_test = features[:test_end_ind]
-    features_test = np.reshape(features_test, (len(features_test), 1))
-    labels_test = labels[:test_end_ind]
-    features_train = features[test_end_ind + 1:]
-    features_train = np.reshape(features_train, (len(features_train), 1))
-    labels_train = labels[test_end_ind + 1:]
+    print "test end index: ", test_end_ind
+    features_test = features[:test_end_ind, :]
+    print "features_test shape: ", features_test.shape
+#    features_test = np.reshape(features_test, (len(features_test), 1))
+    labels_test = labels[:test_end_ind, :]
+    features_train = features[test_end_ind + 1:, :]
+#    features_train = np.reshape(features_train, (len(features_train), 1))
+    labels_train = labels[test_end_ind + 1:, :]
     return features_train, labels_train, features_test, labels_test
