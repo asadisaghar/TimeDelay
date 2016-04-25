d82070d824d0502b52c92a595cb7ba1187bad890
Egil <egil.moller@freecode.no>
2016-04-25 23:53:35 +0200

    cleanup

--- linear_regression_of_fft_diff.py
+++ linear_regression_of_fft_diff.py
@@ -3,19 +3,29 @@ from matplotlib import pyplot as plt
 import sklearn.linear_model
 
 x = np.load("TimeDelayData/fft_comparisons_with_truth.npz")['arr_0']
+np.random.shuffle(x)
 
 xcols = [name for name in x.dtype.names if name.startswith("fft_")]
 
 X = np.zeros((len(x), len(xcols)))
 for i, xcol in enumerate(xcols):
     X[:,i] = x[xcol]
- 
+y = x['dt']
+
+num_rows = len(X)
+train_len = int(num_rows * 3. / 4.)
+Xtrain = X[:train_len,:]
+ytrain = y[:train_len]
+Xtest = X[train_len:,:]
+ytest = y[train_len:]
+
+
 m = sklearn.linear_model.LinearRegression()
-m.fit(X, x['dt'])
+m.fit(Xtrain, ytrain)
 
-ypred = m.predict(X)
+ypred = m.predict(Xtest)
 
-print "MSE: %s" % (sum((x['dt'] - ypred)**2) / len(x),)
+print "MSE: %s" % (sum((ytest - ypred)**2) / len(ypred),)
 
-plt.plot(x['dt'], ypred, '.')
+plt.plot(ytest, ypred, '.')
 plt.show()
