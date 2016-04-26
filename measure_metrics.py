b1a0f0033a2d79edab2016afd6b8f8b931541368
asadisaghar <asadisaghar@gmail.com>
2016-04-26 18:46:00 +0200

    fixed for the output of find_dt_* scripts

--- measure_metrics.py
+++ measure_metrics.py
@@ -1,25 +1,29 @@
 import numpy as np
 
 def measure_metrics(dataset, measurementset):
-    true_val = np.load("TimeDelayData/%s"%(dataset))['arr_0']
-    N_data = np.unique(true_val['pair_id']).shape
-    est_val = np.load("TimeDelayMeasurements/%s"%(measurementset))['arr_0']
-    N_measured = np.unique(est_val['pair_id']).shape
+    fulldata = np.load("TimeDelayData/%s"%(dataset))['arr_0']
+    N_data = np.unique(fulldata['pair_id']).shape
+    data = np.load("TimeDelayMeasurements/%s"%(measurementset))['arr_0']
+    N_measured = np.unique(data['pair_id']).shape
     
+    dt_trues = data['dt']
+    dt_ests = data['est']
+    dt_est_errs = data['est_err']
+
     # success fraction
     f =  N_measured / N_data
 
     # Chi2 value
     normalization = 1. / (f * N)
-    diff = ((est_val['dt'] - true_val['dt']) / est_val['errdt']) ** 2
+    diff = ((dt_ests - dt_trues) / dt_est_err) ** 2
     chi2 = normalization * diff.sum()
 
     # precision
-    precision = est_val['errdt'] / true_val['dt']
-    P = normalization * precision.sum()
+    prec = dt_est_err / dt_trues
+    P = normalization * prec.sum()
 
     # accuracy or bias
-    bias = ((est_val['dt'] - true_val['dt']) / true_val['dt'])
+    bias = ((dt_ests - dt_trues) / dt_trues)
     A = normalization * bias.sum()
 
     return f, chi2, P, A
