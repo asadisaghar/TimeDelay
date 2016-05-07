3d3cd6564e9bf7a33af53f1ed6e0f7837dce786e
Egil <egil.moller@freecode.no>
2016-05-07 22:35:47 +0200

    Save all results of regression

--- regression.py
+++ regression.py
@@ -5,19 +5,23 @@ import sklearn.linear_model
 import numpy.lib.recfunctions
 
 if len(sys.argv) < 2:
-    print """Usage: regression.py input_field1,...,input_fieldN output_field file1 ... fileM"""
+    print """Usage: regression.py input_field1,...,input_fieldN output_field file1 ... fileM output"""
     sys.exit(-1)
 
 input_fields = sys.argv[1].split(",")
 output_field = sys.argv[2]
-inputs = sys.argv[3:]
+inputs = sys.argv[3:-1]
+output = sys.argv[-1]
 
 srccols = []
 y = None
+groundtruth = None
 for i, input in enumerate(inputs):
     data = np.load(input)['arr_0']
     if y is None:
         y = data[output_field]
+    if groundtruth is None:
+        groundtruth = data[['pair_id', 'dt', 'tau', 'sig', 'm1', 'm2']]
     for input_field in input_fields:
         srccols.append(data[input_field])
 
@@ -34,7 +38,13 @@ X = np.zeros((len(data), len(cols)))
 for i, col in enumerate(cols):
     X[:,i] = col
 
-np.random.shuffle(X)
+
+randidx = np.arange(0, len(X), 1)
+np.random.shuffle(randidx)
+
+X = X[randidx]
+y = y[randidx]
+groundtruth = groundtruth[randidx]
 
 num_rows = len(X)
 train_len = int(num_rows * 3. / 4.)
@@ -42,13 +52,24 @@ Xtrain = X[:train_len,:]
 ytrain = y[:train_len]
 Xtest = X[train_len:,:]
 ytest = y[train_len:]
+groundtruthtest = groundtruth[train_len:]
 
 m = sklearn.linear_model.LinearRegression()
 m.fit(Xtrain, ytrain)
+
+with open(output + ".model.pkl", "wb") as f:
+    pickle.dump(m, f)
 
 ypred = m.predict(Xtest)
 
 print "MSE: %s" % (sum((ytest - ypred)**2) / len(ypred),)
 
-plt.plot(ytest, ypred, '.')
-plt.show()
+
+res = np.zeros(len(ypred), dtype=[('pair_id', 'f4'), ('est_dt', 'f4'), ('dt', '<f4'), ('tau', '<f4'), ('sig', '<f4'), ('m1', '<f4'), ('m2', '<f4')])
+
+for name in ('pair_id', 'dt', 'tau', 'sig', 'm1', 'm2'):
+    res[name] = groundtruthtest[name]
+res['est_dt'] = ypred
+
+np.savez(output + ".predictions.npz", x = res)
+
