e5fd27d148ac4074d5efae99e90c1492499b37f3
Egil <egil.moller@freecode.no>
2016-05-08 20:32:41 +0200

    Metrics for heuristic dt

--- measure_metrics.py
+++ measure_metrics.py
@@ -3,19 +3,19 @@ import sys
 
 def measure_metrics(dataset, measurementset):
     fulldata = np.load(dataset)['arr_0']
-    N_data = len(np.unique(fulldata['full_pair_id']))
+    N_data = float(len(np.unique(fulldata['full_pair_id'])))
     data = np.load(measurementset)['arr_0']
-    N_measured = len(np.unique(data['pair_id']))
+    N_measured = float(len(np.unique(data['pair_id'])))
     
     dt_trues = data['dt']
     dt_ests = data['est']
-    dt_est_errs = data['est_err']
+    dt_est_err = data['est_err']
 
     # success fraction
     f =  N_measured / N_data
 
     # Chi2 value
-    normalization = 1. / (f * N)
+    normalization = 1. / (f * N_data)
     diff = ((dt_ests - dt_trues) / dt_est_err) ** 2
     chi2 = normalization * diff.sum()
 
@@ -27,7 +27,13 @@ def measure_metrics(dataset, measurementset):
     bias = ((dt_ests - dt_trues) / dt_trues)
     A = normalization * bias.sum()
 
-    return f, chi2, P, A
+    return {'f': f,
+            'chi2': chi2,
+            'P': P,
+            'A': A}
 
 if __name__ == '__main__':
-    measure_metrics(*sys.argv[1:])
+    for key, value in measure_metrics(*sys.argv[1:]).iteritems():
+        print "%s: %s" % (key, value)
+
+    
